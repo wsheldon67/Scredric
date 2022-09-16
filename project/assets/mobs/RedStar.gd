d54729e0ad399a674f0d43c70da2f46068d0efcf
wsheldon67 <ws.livelaughlove@gmail.com>
2022-09-16 10:09:09 -0500

    Red star drift on playerdead

--- project/assets/mobs/RedStar.gd
+++ project/assets/mobs/RedStar.gd
@@ -5,14 +5,22 @@ var damage = 1
 export var speed_mod = 1
 export var direction = 1
 
+var drifting = Vector2(0, 0)
+
 func _physics_process(delta):
 	if !active:
 		return
 	var player_position = position
 	var closest_player = WORLD.get_closest_player(position)
 	if closest_player:
+		drifting = Vector2(0, 0)
 		player_position = closest_player.position
 	else:
+		if !drifting:
+			drifting.x = rand_range(-1, 1)
+			drifting.y = rand_range(-1, 1)
+		position.x += drifting.x * speed * delta
+		position.y += drifting.y * speed * delta
 		return
 	var m = direction * speed_mod
 	if player_position.x > position.x:
@@ -25,8 +33,7 @@ func _physics_process(delta):
 		position.y -= speed * delta * m
 
 
-
-func _on_Area2D_body_entered(body):
+func _on_RedStar_body_entered(body):
 	if body.is_in_group("players"):
 		body.take_hit(damage)
 		$AnimationPlayer.queue("Attack")
